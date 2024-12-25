2877dd75689105dfc8c26c696b8fba34aecc683b
mubashir-73 <mubashirsheriff7@gmail.com>
2024-12-25 18:43:53 +0530

    Implement React Framework and Boostrap for navbar

---
+++ Landing_page-v2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Nav";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to react</h1>
+    </div>
+  );
+}
+
+export default App;
